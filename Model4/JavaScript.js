043275fda4adf8e59f3033840821ee11f5f452b6
Erson <u6147078902@gmail.com>
2026-02-04 13:59:16 +1000

    LabEX4

---
+++ Model4/JavaScript.js
@@ -0,0 +1,9 @@
+console.log("Hello Erson!");
+
+function addition(a, b) {
+    console.log(4 + 2);
+    console.log(4 - 2);
+    console.log(4 / 2);
+    console.log(4 * 2);
+return a , b;}
+addition(4, 2);
